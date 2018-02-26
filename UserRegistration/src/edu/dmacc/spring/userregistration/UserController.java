8dce796bb444336b2425f6ce53abdd477c5b8568
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2018-02-26 12:15:28 -0600

    added user Results

--- UserRegistration/src/edu/dmacc/spring/userregistration/UserController.java
+++ UserRegistration/src/edu/dmacc/spring/userregistration/UserController.java
@@ -6,17 +6,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-	private static final String[ ] countries = { "France", "United States", "Germany", "England" };
-	
-	
+	private static final String[] countries = { "France", "United States", "Germany", "England" };
+
 	@RequestMapping(value = "/form")
-	public ModelAndView user( ){
-		ModelAndView modelAndView = new ModelAndView( );
-		
+	public ModelAndView user() {
+		ModelAndView modelAndView = new ModelAndView();
+
 		modelAndView.setViewName("userForm");
-		modelAndView.addObject("user", new User( ));
+		modelAndView.addObject("user", new User());
 		modelAndView.addObject("countries", countries);
-		
+
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/result")
+	public ModelAndView processUser(User user) {
+		System.out.println("In processUser");
+		ModelAndView modelAndView = new ModelAndView();
+		System.out.println("Value in getName" + user.getName());
+		modelAndView.setViewName("userResult");
+		modelAndView.addObject("u", user);
 		return modelAndView;
 	}
 
